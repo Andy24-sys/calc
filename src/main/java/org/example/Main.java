fa25391a827283c7d8752019f640107a02ad74ef
Andy24-sys <balans8bak@mail.ru>
2025-02-18 18:32:51 +0300

    Add a*a

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,12 +6,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+
         try {
             String result = calc(input);
             System.out.println(result);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    public static String calc1(int num1, int num2) {
+        int res = num1;
+        for (int i = 0; i < num2 - 1; i++) {
+            res = num1 * res;
+
+        }
+        return String.valueOf(res);
     }
 
     public static String calc(String input) throws Exception {
@@ -32,10 +42,14 @@ public class Main {
         } else if (input.contains(" / ")) {
             arrSymbols = input.split(" / ");
             operator = '/';
+        } else if (input.contains(" ^ ")) {
+            arrSymbols = input.split("\\^ ");
+            operator = '^';
+
         } else {
             throw new Exception("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
         }
-       if (arrSymbols.length != 2) {
+        if (arrSymbols.length != 2) {
             throw new Exception("Некорректный формат ввода. Должны быть два числа и одна операция.");
         }
         int a, b;
@@ -45,7 +59,7 @@ public class Main {
         } catch (NumberFormatException e) {
             throw new Exception("Некорректный ввод чисел. Должны быть целые числа.");
         }
-       if (a < 1 || a > 10 || b < 1 || b > 10) {
+        if (a < 1 || a > 10 || b < 1 || b > 10) {
             throw new Exception("Вне диапазона");
         }
         int result;
@@ -65,9 +79,12 @@ public class Main {
                 }
                 result = a / b;
                 break;
+            case '^':
+                result = Integer.parseInt(calc1(a, b));
+                break;
             default:
                 throw new Exception("Операция невозможна.");
         }
-       return String.valueOf(result);
+        return String.valueOf(result);
     }
 }
